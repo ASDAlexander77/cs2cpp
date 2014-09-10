52ec3265bae6a8e34507fc06320813322adc46a7
Alex D <duzhar@googlemail.com>
2014-09-10 14:07:38 +0100

    fixes for command line

--- Il2Native/Program.cs
+++ Il2Native/Program.cs
@@ -11,14 +11,21 @@
         {
             if (args.Length == 0)
             {
-                Console.Write("usage: ");
-                Console.ForegroundColor = ConsoleColor.White;
-                Console.WriteLine("Il2Bc [file.cs | file.dll] /corelib:<path/corelib.dll> /roslyn");
+                Console.WriteLine("C# Native, https://csnative.codeplex.com/");
+                Console.WriteLine("MSIL to LLVM ByteCode compiler");
+                Console.WriteLine(string.Empty);
+                Console.WriteLine("Usage: Il2Bc [options] file...");
+                Console.WriteLine("file:");
+                Console.WriteLine("  .cs                     C# source file");
+                Console.WriteLine("  .dll                    MSIL dll file");
+                Console.WriteLine("Options:");
+                Console.WriteLine("  /corelib:<file>         Reference standard library (CoreLib.dll)");
+                Console.WriteLine("  /roslyn                 Compile C# source file with Roslyn Compiler");
                 return;
             }
 
             var processedArgs = args.Select(arg => (arg.StartsWith("/") || arg.StartsWith("-")) ? arg.Substring(1) : arg).ToArray();
-            Il2Converter.Convert(args[0], Environment.CurrentDirectory, processedArgs);
+            Il2Converter.Convert(args.First(arg => (!arg.StartsWith("/") && !arg.StartsWith("-"))), Environment.CurrentDirectory, processedArgs);
         }
     }
 }
