59d8f84fb8276bcdfa5ceda40b18c4bb499be9a0
Alex D <duzhar@googlemail.com>
2014-11-27 14:46:40 +0000

    progress

--- Ll2NativeTests/MSTests.cs
+++ Ll2NativeTests/MSTests.cs
@@ -222,7 +222,6 @@ namespace Ll2NativeTests
 
             Debug.Listeners.Clear();
 
-            // last 790
             foreach (var index in Enumerable.Range(1, 907).Where(n => !skip.Contains(n)))
             {
                 Compile(string.Format("test-{0}", index));
