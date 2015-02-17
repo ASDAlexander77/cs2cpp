503c5899aa9c040e3563b3bc6bbc5331e0deefbd
Alex D <duzhar@googlemail.com>
2015-02-17 12:57:19 +0000

    mscolib source added (compilable)

---
+++ mscorlib/src/System/InvalidTimeZoneException.cs
@@ -0,0 +1,25 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace System {
+   using  System.Runtime.Serialization;
+   using  System.Runtime.CompilerServices;
+
+   [Serializable]
+   #if PROTECTION
+[System.Security.Permissions.HostProtection(MayLeakOnAbort = true)]
+#endif
+   [TypeForwardedFrom("System.Core, Version=3.5.0.0, Culture=Neutral, PublicKeyToken=b77a5c561934e089")]
+   public class InvalidTimeZoneException : Exception {
+       public InvalidTimeZoneException(String message)
+           : base(message) { }
+
+       public InvalidTimeZoneException(String message, Exception innerException)
+           : base(message, innerException) { }
+
+       protected InvalidTimeZoneException(SerializationInfo info, StreamingContext context)
+           : base(info, context) { }
+
+       public InvalidTimeZoneException() { }
+   }
+}
---
+++ mscorlib/src/System/Threading/ManualResetEvent.cs
@@ -0,0 +1,28 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+//
+/*=============================================================================
+**
+**
+**
+** Purpose: An example of a WaitHandle class
+**
+**
+=============================================================================*/
+namespace System.Threading {
+    
+    using System;
+    using System.Security.Permissions;
+    using System.Runtime.InteropServices;
+
+    #if PROTECTION
+[HostProtection(Synchronization=true, ExternalThreading=true)]
+#endif
+[System.Runtime.InteropServices.ComVisible(true)]
+    public sealed class ManualResetEvent : EventWaitHandle
+    {        
+        public ManualResetEvent(bool initialState) : base(initialState,EventResetMode.ManualReset){}
+    }
+}
+    
