4a7e122f35faddcb6602f63fe14b244d63a6523a
Alex D <duzhar@googlemail.com>
2015-03-06 18:44:29 +0000

    missing files

---
+++ Il2Native.Logic/Resources/c_declarations.c
@@ -0,0 +1,21 @@
+#ifdef _MSC_VER
+	typedef __int8 int8_t;
+	typedef __int16 int16_t;
+	typedef __int32 int32_t;
+	typedef __int64 int64_t;
+#elif __GNUC__ >= 3
+	typedef signed char int8_t;
+	typedef short int16_t;
+	typedef int int32_t;
+	typedef long long int64_t;
+#endif
+
+typedef bool i1;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+typedef int64_t i64;
+
+typedef i32 (*anyFn)(...);
+
+extern i8* malloc(i32);
---
+++ Il2Native.Logic/Resources/gc_declarations.c
@@ -0,0 +1,5 @@
+extern GC_init();
+extern i8* GC_malloc(i32);
+extern i8* GC_malloc_atomic(i32);
+extern i8* GC_realloc(i8*, i32);
+extern i32 GC_get_heap_size();
