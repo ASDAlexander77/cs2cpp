427cddeb95a2b2d0e0fd87dbc718ae814cac1775
Alex D <duzhar@googlemail.com>
2015-05-05 16:56:29 +0100

    some progress for testing with mscorlib

--- Ll2NativeTests/MSTests.cs
+++ Ll2NativeTests/MSTests.cs
@@ -232,7 +232,7 @@ namespace Ll2NativeTests
         [TestMethod]
         public void TestCompileWithMscorlib()
         {
-            var skip = new List<int>();
+            var skip = new List<int>(new[] { 100 });
             CompilerHelper.Mscorlib = true;
             CompilerHelper.MscorlibPath = @"C:\Windows\Microsoft.NET\assembly\GAC_32\mscorlib\v4.0_4.0.0.0__b77a5c561934e089\mscorlib.dll";
 
@@ -650,6 +650,20 @@ namespace Ll2NativeTests
         public void TestCustomConvert()
         {
             CompilerHelper.ConvertAll("test-1", CompilerHelper.SourcePathCustom);
+        }
+
+        /// <summary>
+        /// </summary>
+        [TestMethod]
+        public void TestGenCompileWithMscorlib()
+        {
+            var skip = new List<int>();
+            CompilerHelper.Mscorlib = true;
+            CompilerHelper.MscorlibPath = @"C:\Windows\Microsoft.NET\assembly\GAC_32\mscorlib\v4.0_4.0.0.0__b77a5c561934e089\mscorlib.dll";
+            foreach (var index in Enumerable.Range(1, 589).Where(n => !skip.Contains(n)))
+            {
+                CompilerHelper.CompileAndRun(string.Format("gtest-{0:000}", index));
+            }
         }
 
         /// <summary>
