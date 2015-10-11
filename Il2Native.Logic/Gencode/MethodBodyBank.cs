db2860b3efaedd41c3226ed0918d0833bd20b027
Alexander <duzhar@hotmail.com>
2015-10-11 20:48:38 +0100

    fix for registering custom methods

--- Il2Native.Logic/Gencode/MethodBodyBank.cs
+++ Il2Native.Logic/Gencode/MethodBodyBank.cs
@@ -38,6 +38,7 @@
                     if (!initialized)
                     {
                         RegisterAll(typeResolver);
+                        initialized = true;
                     }
                 }
             }
