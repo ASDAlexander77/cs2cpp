b9f83d530cfbd44f406b50fbb7e4d03859c11492
Alexander <duzhar@hotmail.com>
2015-10-11 23:21:31 +0100

    removed test debug.assert

--- Il2Native.Logic/CWriter.cs
+++ Il2Native.Logic/CWriter.cs
@@ -2732,8 +2732,6 @@ namespace Il2Native.Logic
 
         public void WriteMethod(IMethod method, IMethod methodOpCodeHolder, IGenericContext genericMethodContext)
         {
-            Debug.Assert(method.Name != "FastAllocateString");
-
             if (method is IConstructor && method.IsStatic)
             {
                 this.IlReader.StaticConstructors.Add(method);
