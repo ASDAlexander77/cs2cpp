9a046bae0c12ec7a5931eeb69749610ce2e60f95
Alexander <duzhar@hotmail.com>
2015-10-14 22:44:26 +0100

    progress

---
+++ CoreLib/Impl/Environment.cs
@@ -0,0 +1,50 @@
+﻿namespace System
+{
+    using Runtime.CompilerServices;
+
+    [MergeCode]
+    public static partial class Environment
+    {
+        [MergeCode]
+        private const int CLOCK_MONOTONIC = 1;
+
+        [MethodImplAttribute(MethodImplOptions.Unmanaged)]
+        [MergeCode]
+        public static extern unsafe int clock_gettime(int type, int* time);
+
+        [MethodImpl(MethodImplOptions.Unmanaged)]
+        [MergeCode]
+        public static extern void exit(int exitCode);
+
+        [MergeCode]
+        internal static String GetResourceFromDefault(String key)
+        {
+            return key;
+        }
+
+        [MergeCode]
+        public static int TickCount
+        {
+            get
+            {
+                unsafe
+                {
+                    long time;
+                    if (clock_gettime(CLOCK_MONOTONIC, (int*)&time) == 0)
+                    {
+                        return (int)time >> 32;
+                    }
+
+                    throw new Exception();
+                }
+            }
+        }
+
+        // Terminates this process with the given exit code.
+        [MergeCode]
+        internal static void _Exit(int exitCode)
+        {
+            __exit(exitCode);
+        }
+    }
+}
