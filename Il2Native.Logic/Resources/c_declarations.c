460f5a9cc5fc68d6af6531e53423e9502ff8c822
Alex Duzhar <duzhar@hotmail.com>
2016-01-25 11:12:01 +0000

    fix for declaring literal operator

--- Il2Native.Logic/Resources/c_declarations.c
+++ Il2Native.Logic/Resources/c_declarations.c
@@ -1,3 +1,8 @@
 template <typename T> class __array : public <<%assemblyName%>>::System::Array
 {
 };
+
+inline string* operator "" _s(const wchar_t* str, size_t len)
+{
+	return new string((wchar_t*)str, 0, (int32_t)len);
+}
