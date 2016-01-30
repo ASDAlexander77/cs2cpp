5bd9aea445e0a21321515ab8e7b6fc0b2399f4ab
Alexander <duzhar@hotmail.com>
2016-01-30 15:53:23 +0000

    refactoring for prefix and postfix operations

--- Il2Native.Logic/DOM2/PostfixUnaryExpression.cs
+++ Il2Native.Logic/DOM2/PostfixUnaryExpression.cs
@@ -1,57 +1,13 @@
 ﻿namespace Il2Native.Logic.DOM2
 {
-    using System;
-    using System.Linq;
-
     using Microsoft.CodeAnalysis.CSharp;
-    using Microsoft.CodeAnalysis.CSharp.Syntax.InternalSyntax;
 
-    public class PostfixUnaryExpression : Expression
+    public class PostfixUnaryExpression : PrefixPostfixUnaryExpressionBase
     {
-        private Expression value;
-
-        private SyntaxKind operatorKind;
-
-        internal bool Parse(BoundSequence boundSequence)
-        {
-            base.Parse(boundSequence);
-
-            if (boundSequence.SideEffects.Length > 2 || !boundSequence.SideEffects.All(se => se is BoundAssignmentOperator))
-            {
-                return false;
-            }
-
-            var boundAssignmentOperator = boundSequence.SideEffects.Last() as BoundAssignmentOperator;
-            if (boundAssignmentOperator != null)
-            {
-                this.value = Deserialize(boundAssignmentOperator.Left) as Expression;
-            }
-            
-            var postfixUnaryExpressionSyntax = boundSequence.Syntax.Green as PostfixUnaryExpressionSyntax;
-            if (postfixUnaryExpressionSyntax != null)
-            {
-                this.operatorKind = postfixUnaryExpressionSyntax.OperatorToken.Kind;
-            }
-
-            var call = value as Call;
-            if (call != null && (call.Method.Name == "op_Increment" || call.Method.Name == "op_Decrement"))
-            {
-                return false;
-            }
-
-            if (value == null)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
         internal override void WriteTo(CCodeWriterBase c)
         {
-            var call = value as Call;
-            this.value.WriteTo(c);
-            switch (this.operatorKind)
+            Value.WriteTo(c);
+            switch (OperatorKind)
             {
                 case SyntaxKind.PlusPlusToken:
                     c.TextSpan("++");
--- Il2Native.Logic/DOM2/PrefixUnaryExpression.cs
+++ Il2Native.Logic/DOM2/PrefixUnaryExpression.cs
@@ -1,55 +1,12 @@
 ﻿namespace Il2Native.Logic.DOM2
 {
-    using System;
-    using System.Linq;
-
     using Microsoft.CodeAnalysis.CSharp;
-    using Microsoft.CodeAnalysis.CSharp.Syntax.InternalSyntax;
 
-    public class PrefixUnaryExpression : Expression
+    public class PrefixUnaryExpression : PrefixPostfixUnaryExpressionBase
     {
-        private Expression value;
-
-        private SyntaxKind operatorKind;
-
-        internal bool Parse(BoundSequence boundSequence)
-        {
-            base.Parse(boundSequence);
-
-            if (boundSequence.SideEffects.Length > 2 || !boundSequence.SideEffects.All(se => se is BoundAssignmentOperator))
-            {
-                return false;
-            }
-
-            var boundAssignmentOperator = boundSequence.SideEffects.First() as BoundAssignmentOperator;
-            if (boundAssignmentOperator != null)
-            {
-                this.value = Deserialize(boundAssignmentOperator.Right) as Expression;
-            }
-
-            var prefixUnaryExpressionSyntax = boundSequence.Syntax.Green as PrefixUnaryExpressionSyntax;
-            if (prefixUnaryExpressionSyntax != null)
-            {
-                this.operatorKind = prefixUnaryExpressionSyntax.OperatorToken.Kind;
-            }
-
-            var call = value as Call;
-            if (call != null && (call.Method.Name == "op_Increment" || call.Method.Name == "op_Decrement"))
-            {
-                return false;
-            }
-
-            if (value == null)
-            {
-                return false;
-            }
-
-            return true;
-        }
-
         internal override void WriteTo(CCodeWriterBase c)
         {
-            switch (this.operatorKind)
+            switch (OperatorKind)
             {
                 case SyntaxKind.PlusPlusToken:
                     c.TextSpan("++");
@@ -59,7 +16,7 @@
                     break;
             }
 
-            this.value.WriteTo(c);
+            Value.WriteTo(c);
         }
     }
 }
