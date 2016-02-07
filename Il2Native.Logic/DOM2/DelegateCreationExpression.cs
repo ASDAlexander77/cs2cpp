d09f9d890e6a0d97c772686dd2426f48737999cb
Alexander <duzhar@hotmail.com>
2016-02-07 22:55:23 +0000

    delegate fix

--- Il2Native.Logic/DOM2/DelegateCreationExpression.cs
+++ Il2Native.Logic/DOM2/DelegateCreationExpression.cs
@@ -10,7 +10,17 @@
             base.Parse(boundDelegateCreationExpression);
             var argument = Deserialize(boundDelegateCreationExpression.Argument) as Expression;
             Debug.Assert(argument != null);
-            Arguments.Add(argument);
+
+            if (boundDelegateCreationExpression.MethodOpt != null)
+            {
+                var methodGroup = new MethodGroup { ReceiverOpt = argument };
+                methodGroup.Methods.Add(boundDelegateCreationExpression.MethodOpt);
+                Arguments.Add(methodGroup);
+            }
+            else
+            {
+                Arguments.Add(argument);
+            }
         }
 
         internal override void WriteTo(CCodeWriterBase c)
