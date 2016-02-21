c920f6a0ab642a8bc5d89b8ddd17a0c2a6a36823
Alexander <duzhar@hotmail.com>
2016-02-21 11:30:15 +0000

    fix

--- Il2Native.Logic/impl/System/Threading/Interlocked.h
+++ Il2Native.Logic/impl/System/Threading/Interlocked.h
@@ -1,14 +1,14 @@
 
 // Method : System.Threading.Interlocked.Exchange<T>(ref T, T)
 template <typename T> 
-T CoreLib::System::Threading::Interlocked::Exchange_Ref<T>(T& location1, T value)
+T CoreLib::System::Threading::Interlocked::Exchange_Ref(T& location1, T value)
 {
     throw 3221274624U;
 }
 
 // Method : System.Threading.Interlocked.CompareExchange<T>(ref T, T, T)
 template <typename T> 
-T CoreLib::System::Threading::Interlocked::CompareExchange_Ref<T>(T& location1, T value, T comparand)
+T CoreLib::System::Threading::Interlocked::CompareExchange_Ref(T& location1, T value, T comparand)
 {
     return __sync_val_compare_and_swap(&location1, value, comparand);
 }
