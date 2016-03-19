5b227b7be26fefc79c4d0b4aa914df75beaf1ce2
Alexander <duzhar@hotmail.com>
2016-03-19 20:36:05 +0000

    fixes

--- Il2Native.Logic/impl/System/Threading/Interlocked.h
+++ Il2Native.Logic/impl/System/Threading/Interlocked.h
@@ -17,7 +17,8 @@ T CoreLib::System::Threading::Interlocked::CompareExchange_Ref(T& location1, T v
 	return OSAtomicCompareAndSwapPtr (comparand, value, &location1);
 #elif _WIN32 && _MSC_VER
 	auto local_comparand = comparand;
-	return _Atomic_compare_exchange_strong_4((volatile std::_Uint4_t*)&location1, (std::_Uint4_t*)&local_comparand, value);
+	std::_Atomic_compare_exchange_strong_4((volatile std::_Uint4_t*)&location1, (std::_Uint4_t*)&local_comparand, (std::_Uint4_t)value, std::memory_order_relaxed, std::memory_order_relaxed);
+	return local_comparand;
 #elif (__GNUC__ * 10000 + __GNUC_MINOR__ * 100 + __GNUC_PATCHLEVEL__) > 40100
 	return __sync_val_compare_and_swap(&location1, comparand, value);
 #else
