8a2d1f72a6dd216194a875031a25d8b8372abfd9
Alex Duzhar <duzhar@hotmail.com>
2016-03-29 13:38:26 +0100

    implementing date now

--- Il2Native.Logic/impl/System/DateTime.cpp
+++ Il2Native.Logic/impl/System/DateTime.cpp
@@ -3,11 +3,11 @@
 // Method : System.DateTime.GetSystemTimeAsFileTime()
 int64_t CoreLib::System::DateTime::GetSystemTimeAsFileTime()
 {
-/*
-	auto now = std::chrono::time_point_cast<std::chrono::seconds>(std::chrono::system_clock::now());
+	auto now = std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now());
 	auto epoch = now.time_since_epoch();
-	auto value = std::chrono::duration_cast<std::chrono::seconds>(epoch);
-    return (int64_t) value.count();
-*/
-    return 0;
+	auto value = std::chrono::duration_cast<std::chrono::nanoseconds>(epoch);
+	auto val = (int64_t) value.count();
+
+	auto val_system = (val / 100) + 116436096000000000 + 8639999870000;
+	return val_system;
 }
