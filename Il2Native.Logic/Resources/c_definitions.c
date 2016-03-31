5c3d55d84404cac8cba657fd2c5defb8b4ce9ac5
Alex Duzhar <duzhar@hotmail.com>
2016-03-31 11:23:03 +0100

    experiments

--- Il2Native.Logic/Resources/c_definitions.c
+++ Il2Native.Logic/Resources/c_definitions.c
@@ -7,4 +7,4 @@ string* operator "" _s(const wchar_t* ptr, size_t length)
 }
 
 std::unordered_map<void*, std::timed_mutex> __locks;
-std::unordered_map<void*, std::condition_variable_any> __conditions;
+std::unordered_map<void*, std::condition_variable> __conditions;
--- Il2Native.Logic/impl/System/Threading/Monitor.cpp
+++ Il2Native.Logic/impl/System/Threading/Monitor.cpp
@@ -9,24 +9,44 @@ void CoreLib::System::Threading::Monitor::Enter(object* obj)
 // Method : System.Threading.Monitor.ReliableEnter(object, ref bool)
 void CoreLib::System::Threading::Monitor::ReliableEnter_Ref(object* obj, bool& lockTaken)
 {
+	if (obj == nullptr)
+	{
+		throw __new<CoreLib::System::ArgumentNullException>();
+	}
+
 	lockTaken = __locks[(void*)obj].try_lock_for(std::chrono::milliseconds::max());
 }
 
 // Method : System.Threading.Monitor.Exit(object)
 void CoreLib::System::Threading::Monitor::Exit(object* obj)
 {
+	if (obj == nullptr)
+	{
+		throw __new<CoreLib::System::ArgumentNullException>();
+	}
+
     __locks[(void*)obj].unlock();
 }
 
 // Method : System.Threading.Monitor.ReliableEnterTimeout(object, int, ref bool)
 void CoreLib::System::Threading::Monitor::ReliableEnterTimeout_Ref(object* obj, int32_t timeout, bool& lockTaken)
 {
+	if (obj == nullptr)
+	{
+		throw __new<CoreLib::System::ArgumentNullException>();
+	}
+
     lockTaken = __locks[(void*)obj].try_lock_for(std::chrono::milliseconds(timeout));
 }
 
 // Method : System.Threading.Monitor.IsEnteredNative(object)
 bool CoreLib::System::Threading::Monitor::IsEnteredNative(object* obj)
 {
+	if (obj == nullptr)
+	{
+		throw __new<CoreLib::System::ArgumentNullException>();
+	}
+
     auto lockTaken = __locks[(void*)obj].try_lock_for(std::chrono::milliseconds(1));
 	if (lockTaken)
 	{
@@ -39,21 +59,33 @@ bool CoreLib::System::Threading::Monitor::IsEnteredNative(object* obj)
 // Method : System.Threading.Monitor.ObjWait(bool, int, object)
 bool CoreLib::System::Threading::Monitor::ObjWait(bool exitContext, int32_t millisecondsTimeout, object* obj)
 {
-	return __conditions[(void*)obj].wait_for(
-		__locks[(void*)obj], 
-		millisecondsTimeout == -1 
-			? std::chrono::milliseconds::max() 
-			: std::chrono::milliseconds(millisecondsTimeout)) == std::cv_status::no_timeout;
+	if (obj == nullptr)
+	{
+		throw __new<CoreLib::System::ArgumentNullException>();
+	}
+
+	std::unique_lock<std::timed_mutex> lk(__locks[(void*)obj]);
+	return std::cv_status::no_timeout == __conditions[(void*)obj].wait_for(lk, millisecondsTimeout == -1 ? std::chrono::milliseconds::max() : std::chrono::milliseconds(millisecondsTimeout));
 }
 
 // Method : System.Threading.Monitor.ObjPulse(object)
 void CoreLib::System::Threading::Monitor::ObjPulse(object* obj)
 {
+	if (obj == nullptr)
+	{
+		throw __new<CoreLib::System::ArgumentNullException>();
+	}
+
 	__conditions[(void*)obj].notify_one();
 }
 
 // Method : System.Threading.Monitor.ObjPulseAll(object)
 void CoreLib::System::Threading::Monitor::ObjPulseAll(object* obj)
 {
+	if (obj == nullptr)
+	{
+		throw __new<CoreLib::System::ArgumentNullException>();
+	}
+
 	__conditions[(void*)obj].notify_all();
 }
