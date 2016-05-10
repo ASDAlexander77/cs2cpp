80c8771b4c73dba95de11fdf8de0754db16e3ff9
Alex Duzhar <duzhar@hotmail.com>
2016-05-10 15:39:13 +0100

    fixed Math for android

--- Il2Native.Logic/impl/System/Math.cpp
+++ Il2Native.Logic/impl/System/Math.cpp
@@ -3,125 +3,129 @@
 // Method : System.Math.Acos(double)
 double CoreLib::System::Math::Acos(double d)
 {
-    return std::acos(d);
+	return std::acos(d);
 }
 
 // Method : System.Math.Asin(double)
 double CoreLib::System::Math::Asin(double d)
 {
-    return std::asin(d);
+	return std::asin(d);
 }
 
 // Method : System.Math.Atan(double)
 double CoreLib::System::Math::Atan(double d)
 {
-    return std::atan(d);
+	return std::atan(d);
 }
 
 // Method : System.Math.Atan2(double, double)
 double CoreLib::System::Math::Atan2(double y, double x)
 {
-    return std::atan2(y, x);
+	return std::atan2(y, x);
 }
 
 // Method : System.Math.Ceiling(double)
 double CoreLib::System::Math::Ceiling(double a)
 {
-    return std::ceil(a);
+	return std::ceil(a);
 }
 
 // Method : System.Math.Cos(double)
 double CoreLib::System::Math::Cos(double d)
 {
-    return std::cos(d);
+	return std::cos(d);
 }
 
 // Method : System.Math.Cosh(double)
 double CoreLib::System::Math::Cosh(double value)
 {
-    return std::cosh(value);
+	return std::cosh(value);
 }
 
 // Method : System.Math.Floor(double)
 double CoreLib::System::Math::Floor(double d)
 {
-    return std::floor(d);
+	return std::floor(d);
 }
 
 // Method : System.Math.Sin(double)
 double CoreLib::System::Math::Sin(double a)
 {
-    return std::sin(a);
+	return std::sin(a);
 }
 
 // Method : System.Math.Tan(double)
 double CoreLib::System::Math::Tan(double a)
 {
-    return std::tan(a);
+	return std::tan(a);
 }
 
 // Method : System.Math.Sinh(double)
 double CoreLib::System::Math::Sinh(double value)
 {
-    return std::sinh(value);
+	return std::sinh(value);
 }
 
 // Method : System.Math.Tanh(double)
 double CoreLib::System::Math::Tanh(double value)
 {
-    return std::tanh(value);
+	return std::tanh(value);
 }
 
 // Method : System.Math.Round(double)
 double CoreLib::System::Math::Round(double a)
 {
-    return std::round(a);
+#if defined(__ANDROID__) || defined(PLATFORM_ANDROID)
+	return std::floor(a + 0.5);
+#else
+	return std::round(a);
+#endif
 }
 
 // Method : System.Math.SplitFractionDouble(double*)
 double CoreLib::System::Math::SplitFractionDouble(double* value)
 {
-    throw 0xC000C000;
+	throw 0xC000C000;
 }
 
 // Method : System.Math.Sqrt(double)
 double CoreLib::System::Math::Sqrt(double d)
 {
-    return std::sqrt(d);
+	return std::sqrt(d);
 }
 
 // Method : System.Math.Log(double)
 double CoreLib::System::Math::Log(double d)
 {
-    return std::log(d);
+	return std::log(d);
 }
 
 // Method : System.Math.Log10(double)
 double CoreLib::System::Math::Log10(double d)
 {
-    return std::log10(d);
+	return std::log10(d);
 }
 
 // Method : System.Math.Exp(double)
 double CoreLib::System::Math::Exp(double d)
 {
-    return std::exp(d);
+	return std::exp(d);
 }
 
 // Method : System.Math.Pow(double, double)
 double CoreLib::System::Math::Pow(double x, double y)
 {
-    return std::pow(x, y);
+	return std::pow(x, y);
 }
 
 // Method : System.Math.Abs(float)
 float CoreLib::System::Math::Abs(float value)
 {
-    return std::abs(value);
+	return std::abs(value);
 }
 
 // Method : System.Math.Abs(double)
 double CoreLib::System::Math::Abs(double value)
 {
-    return std::abs(value);
+	return std::abs(value);
 }
