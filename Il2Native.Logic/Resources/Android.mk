e2fd6542985e007f9cd099f8dcb3629969736dda
Alexander <duzhar@hotmail.com>
2016-05-11 23:47:04 +0100

    some fixes

--- Il2Native.Logic/Resources/Android.mk
+++ Il2Native.Logic/Resources/Android.mk
@@ -17,6 +17,6 @@ LOCAL_SRC_FILES := $(FILE_LIST:$(LOCAL_PATH)/%=%)
 
 LOCAL_C_INCLUDES := $(SRC_PATH) $(LOCAL_PATH)/../../bdwgc/include
 				   
-LOCAL_CPPFLAGS += -Wno-write-strings -Wno-conversion-null -Wno-invalid-offsetof -fpermissive
+LOCAL_CPPFLAGS += -Wno-write-strings -Wno-conversion-null -Wno-invalid-offsetof
 
 include $(BUILD_STATIC_LIBRARY)
