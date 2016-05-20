7ba7754e809160da8b9b70c582cf48a009d586d8
Alex Duzhar <duzhar@hotmail.com>
2016-05-20 16:12:32 +0100

    fixes

--- Il2Native.Logic/impl/System/Environment.cpp
+++ Il2Native.Logic/impl/System/Environment.cpp
@@ -9,15 +9,9 @@ int32_t CoreLib::System::Environment::GetProcessorCount()
 // Method : System.Environment.TickCount.get
 int32_t CoreLib::System::Environment::get_TickCount()
 {
-	auto now = std::chrono::time_point_cast<std::chrono::nanoseconds>(std::chrono::system_clock::now());
-	auto epoch = now.time_since_epoch();
-	auto value = std::chrono::duration_cast<std::chrono::nanoseconds>(epoch);
-	auto val = (int64_t)value.count();
-
-	auto val_ticksSince1Jan1970 = val / 10; // ticks since 1 Jan 1970
-	auto val_since0 = val_ticksSince1Jan1970 + 621671328000000000;
-	auto val_since1601 = val_since0 - 504911232000000000;
-	return val_since1601 / 10; // 100-nano seconds
+	std::chrono::steady_clock::time_point t0;
+	auto now = std::chrono::steady_clock::now() - t0;
+	return now.count() / 100;
 }
 
 // Method : System.Environment._Exit(int)
